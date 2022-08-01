e6cb5167a60e76cee94008f723ad06b74d221706
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-08-01 12:55:38 -0400

    initial code

---
+++ README.md
@@ -0,0 +1,45 @@
+# JSON Schema Walker
+
+Loosely based on [CloudFlare's json schema tools](https://github.com/cloudflare/json-schema-tools/tree/master/workspaces/json-schema-walker)
+
+A system that visits all schema objects in a JSON Schema document and makes callbacks before and/or after visiting all of the current schema object's subschemas.
+
+## Usage
+
+```typescript
+import { Walker } from "json-schema-walker";
+const schema = {
+  // your json schema
+};
+const walker = new Walker<T>();
+await walker.loadSchema(schema, {
+  cloneSchema: true,
+  dereference: false,
+  dereferenceOptions: {
+    dereference: {
+      circular: "ignore",
+    },
+  },
+});
+const convertSchema = (schema) => {
+  // do something with the schema properties
+};
+await walker.walk(convertSchema, walker.vocabularies.DRAFT_07);
+const updatedSchema = walker.rootSchema;
+```
+
+## Circular references
+
+Passing the options
+
+```json
+{
+  "dereferenceOptions": {
+    "dereference": {
+      "circular": "ignore"
+    }
+  }
+}
+```
+
+will dereference all non-circular references in your schema
---
+++ src/index.ts
@@ -0,0 +1,231 @@
+import type { JSONSchema } from "@apidevtools/json-schema-ref-parser";
+import clone from "clone";
+import RefParser from "@apidevtools/json-schema-ref-parser";
+import type $RefParser from "@apidevtools/json-schema-ref-parser";
+import type { JSONSchema4 } from "json-schema";
+import type { JSONSchema6, JSONSchema7 } from "json-schema";
+
+const visited: unique symbol = Symbol("visited");
+const NEXT_SCHEMA_KEYWORD: unique symbol = Symbol("NEXT_SCHEMA_KEYWORD");
+const NEXT_LDO_KEYWORD: unique symbol = Symbol("NEXT_LDO_KEYWORD");
+
+type InputSchema = JSONSchema;
+
+type ProcessorFunction<T> = (schema: T) => void;
+
+type ProcessorFunctionInternal = (schema: ISubSchema, keyword: string | number) => void;
+
+type IVocabulary = Record<string, any>;
+type ISubSchema = Record<string, any>;
+interface Options<T extends InputSchema> {
+  cloneSchema?: boolean;
+  dereference?: boolean;
+  dereferenceOptions?: $RefParser.Options;
+}
+export class Walker<T extends InputSchema = InputSchema> {
+  rootSchema!: T;
+  vocabulary!: IVocabulary;
+  vocabularies!: Record<string, IVocabulary>;
+  walker!: ProcessorFunction<T>;
+
+  constructor() {
+    this.initVocabulary();
+  }
+
+  loadSchema = async (schema: T, options?: Options<T>) => {
+    const { cloneSchema = true, dereference = false, dereferenceOptions } = options || {};
+    this.rootSchema = cloneSchema ? clone(schema) : schema;
+    if (dereference) {
+      const parser = new RefParser();
+      this.rootSchema = (await parser.dereference(schema, dereferenceOptions || {})) as T;
+    }
+  };
+
+  walk = async (processor: ProcessorFunction<T>, vocabulary: IVocabulary) => {
+    this.vocabulary = vocabulary ?? this.vocabularies.DRAFT_07;
+    this.walker = processor;
+    this.walker(this.rootSchema);
+    await this.subschemaWalk(this.rootSchema);
+    // clean up the symbols we injected to check for circular references
+    this.cleanupVisited(this.rootSchema);
+  };
+
+  private cleanupVisited = (schema: ISubSchema) => {
+    for (const entry of Object.values(schema)) {
+      if (typeof entry === "object" && entry[visited]) {
+        delete entry[visited];
+        this.cleanupVisited(entry);
+      }
+    }
+  };
+
+  private isValidSubSchema = (schema: unknown) =>
+    (schema instanceof Object && !Array.isArray(schema)) || typeof schema === "boolean";
+
+  private applyUserProcessor = (schema: ISubSchema, key: string | number) => {
+    const schemaElement = schema[key];
+    if (typeof schemaElement !== "object") {
+      return;
+    }
+    schemaElement[visited] = true;
+
+    this.walker(schemaElement);
+    this.subschemaWalk(schemaElement);
+  };
+
+  private subschemaWalk = (schema: ISubSchema) => {
+    for (const keyword in schema) {
+      try {
+        this.processSchemaKey(schema, keyword);
+      } catch (e) {
+        if (e !== NEXT_SCHEMA_KEYWORD) {
+          throw e;
+        }
+      }
+    }
+  };
+
+  // These are the processors
+  private processSchemaKey = (schema: ISubSchema, keyword: string) => {
+    if (typeof schema[keyword] !== "object") {
+      return;
+    }
+    const processorFunction = this.vocabulary[keyword];
+    if (!processorFunction) {
+      return;
+    }
+    schema[keyword][visited] = true;
+    processorFunction(schema, keyword);
+  };
+  private processObjectOfSchemas = (schema: ISubSchema, keyword: string) => {
+    for (const prop of Object.getOwnPropertyNames(schema[keyword])) {
+      if (typeof schema[keyword][prop] === "object") {
+        this.applyUserProcessor(schema[keyword], prop);
+      }
+    }
+  };
+  private processArrayOfSchemas = (schema: ISubSchema, keyword: string) => {
+    for (const prop of Object.getOwnPropertyNames(schema[keyword])) {
+      if (typeof schema[keyword][prop] === "object") {
+        this.applyUserProcessor(schema[keyword], prop);
+      }
+    }
+    for (let i = 0; i < schema[keyword].length; i++) {
+      this.applyUserProcessor(schema[keyword], i);
+    }
+  };
+  private processSingleOrArrayOfSchemas = (schema: ISubSchema, keyword: string) => {
+    if (this.isValidSubSchema(schema[keyword])) {
+      this.processSingleSchema(schema, keyword);
+    } else {
+      this.processArrayOfSchemas(schema, keyword);
+    }
+  };
+
+  private processSingleSchema = (schema: ISubSchema, keyword: string) => {
+    this.applyUserProcessor(schema, keyword);
+  };
+
+  /**
+   * Loop over the links and apply the callbacks, while
+   * handling LDO keyword deletions by catching NEXT_LDO_KEYWORD.
+   */
+  private getProcessLinks = (ldoVocabulary: IVocabulary) => {
+    return (schema: ISubSchema, keyword: string | number) => {
+      for (const ldo of schema.links) {
+        for (const key in ldo) {
+          try {
+            ldoVocabulary[keyword]?.(schema, key);
+          } catch (e) {
+            if (e !== NEXT_LDO_KEYWORD) {
+              throw e;
+            }
+          }
+        }
+      }
+    };
+  };
+
+  // vocabulary initialization
+  private initVocabulary = () => {
+    const DRAFT_04 = {
+      properties: this.processObjectOfSchemas,
+      patternProperties: this.processObjectOfSchemas,
+      additionalProperties: this.processSingleSchema,
+      dependencies: this.processObjectOfSchemas,
+      items: this.processSingleOrArrayOfSchemas,
+      additionalItems: this.processSingleSchema,
+      allOf: this.processArrayOfSchemas,
+      anyOf: this.processArrayOfSchemas,
+      oneOf: this.processArrayOfSchemas,
+      not: this.processSingleSchema,
+      if: this.processSingleSchema,
+      then: this.processSingleSchema,
+      else: this.processSingleSchema,
+    } as Record<keyof JSONSchema4, ProcessorFunctionInternal>;
+
+    /**
+     * LDO keywords call _apply directly as they have a different
+     * mapping from the schema keyword into the path that _apply
+     * expects.  This is done in the function returned from
+     * _getProcessLinks();
+     */
+    const DRAFT_04_HYPER_LDO = {
+      schema: this.applyUserProcessor,
+      targetSchema: this.applyUserProcessor,
+    };
+
+    const DRAFT_04_HYPER = {
+      ...DRAFT_04,
+      links: this.getProcessLinks(DRAFT_04_HYPER_LDO),
+    } as Record<string, ProcessorFunctionInternal>;
+
+    const DRAFT_06 = {
+      ...DRAFT_04,
+      propertyNames: this.processObjectOfSchemas,
+    } as Record<keyof JSONSchema6, ProcessorFunctionInternal>;
+
+    const DRAFT_06_HYPER_LDO = {
+      hrefSchema: this.applyUserProcessor,
+      targetSchema: this.applyUserProcessor,
+      submissionSchema: this.applyUserProcessor,
+    };
+
+    const DRAFT_06_HYPER = {
+      ...DRAFT_06,
+      links: this.getProcessLinks(DRAFT_06_HYPER_LDO),
+    } as Record<keyof JSONSchema6, ProcessorFunctionInternal>;
+
+    const DRAFT_07 = { ...DRAFT_06 } as Record<keyof JSONSchema7, ProcessorFunctionInternal>;
+
+    const DRAFT_07_HYPER_LDO = {
+      ...DRAFT_06_HYPER_LDO,
+      headerSchema: this.applyUserProcessor,
+    } as Record<string, ProcessorFunctionInternal>;
+
+    const DRAFT_07_HYPER = {
+      ...DRAFT_07,
+      links: this.getProcessLinks(DRAFT_07_HYPER_LDO),
+    } as Record<string, ProcessorFunctionInternal>;
+
+    const CLOUDFLARE_DOCA = {
+      ...DRAFT_04,
+      links: this.getProcessLinks({
+        ...DRAFT_04_HYPER_LDO,
+        ...DRAFT_07_HYPER_LDO,
+      }),
+    } as Record<string, ProcessorFunctionInternal>;
+    this.vocabularies = {
+      DRAFT_04,
+      DRAFT_04_HYPER,
+      DRAFT_04_HYPER_LDO,
+      DRAFT_06,
+      DRAFT_06_HYPER,
+      DRAFT_06_HYPER_LDO,
+      DRAFT_07,
+      DRAFT_07_HYPER,
+      DRAFT_07_HYPER_LDO,
+      CLOUDFLARE_DOCA,
+    } as const;
+  };
+}
