51f3a2cea9d7b04d52ad5cc724232125ea2472e7
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-08-01 17:08:40 -0400

    bump version, support top level derefs

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,39 @@ interface Options<T extends InputSchema> {
   dereference?: boolean;
   dereferenceOptions?: $RefParser.Options;
 }
+
+/**
+ * This is a hotfix and really only a partial solution as it does not cover all cases.
+ *
+ * But it's the best we can do until we find or build a better library to handle references.
+ *
+ * original source https://github.com/asyncapi/modelina/pull/829/files
+ */
+const handleRootReference = <T extends Record<string, any> = Record<string, any>>(input: Record<string, any>): T => {
+  //Because of https://github.com/APIDevTools/json-schema-ref-parser/issues/201 the tool cannot handle root references.
+  //This really is a bad patch to fix an underlying problem, but until a full library is available, this is best we can do.
+  const hasRootRef = input.$ref !== undefined;
+  if (hasRootRef) {
+    //When we encounter it, manually try to resolve the reference in the definitions section
+    const hasDefinitionSection = input.definitions !== undefined;
+    if (hasDefinitionSection) {
+      const definitionLink = "#/definitions/";
+      const referenceLink = input.$ref.slice(0, definitionLink.length);
+      const referenceIsLocal = referenceLink === definitionLink;
+      if (referenceIsLocal) {
+        const definitionName = input.$ref.slice(definitionLink.length);
+        const definition = input.definitions[String(definitionName)];
+        const definitionExist = definition !== undefined;
+        if (definitionExist) {
+          delete input.$ref;
+          return { ...definition, ...input };
+        }
+      }
+    }
+  }
+  return input as T;
+};
+
 export class Walker<T extends InputSchema = InputSchema> {
   rootSchema!: T;
   vocabulary!: IVocabulary;
@@ -38,6 +71,7 @@ export class Walker<T extends InputSchema = InputSchema> {
     if (dereference) {
       const parser = new RefParser();
       this.rootSchema = (await parser.dereference(schema, dereferenceOptions || {})) as T;
+      this.rootSchema = handleRootReference<T>(this.rootSchema);
     }
   };
 
